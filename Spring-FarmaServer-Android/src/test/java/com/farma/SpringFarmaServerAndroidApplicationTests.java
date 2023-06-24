f00127eeb69e8f224ebcdfde5b42be7b00d87021
JosephFz <84686267+JosephFz@users.noreply.github.com>
2023-06-23 19:12:34 -0500

    Add files via upload

---
+++ Spring-FarmaServer-Android/src/test/java/com/farma/SpringFarmaServerAndroidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.farma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFarmaServerAndroidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
